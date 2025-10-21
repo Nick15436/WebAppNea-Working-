ba4dc8e7aad3f283950da0e2fec08cb84702d3a1
Nick Kindzhakov <kindzhakov08@gmail.com>
2025-10-21 20:05:11 +0300

    fixed problem with the changing datatypes of the ApiData. Manually added data, so that chart still works when there are no free requests left.

--- TimeSeriesDataPoint.cs
+++ TimeSeriesDataPoint.cs
@@ -7,19 +7,19 @@ public class TimeSeriesDataPoint
 {
     //Each JSON datapoint from the API response has all of these properties. This is a way to get these properties for each individual datapoint.
     [JsonPropertyName("1. open")]
-    public float Open { get; set; }
+    public string Open { get; set; }
     
     [JsonPropertyName("2. high")]
-    public float High { get; set; }
+    public string High { get; set; }
     
     [JsonPropertyName("3. low")]
-    public float Low { get; set; }
+    public string Low { get; set; }
     
     [JsonPropertyName("4. close")]
-    public float Close { get; set; }
+    public string Close { get; set; }
     
     [JsonPropertyName("5. volume")]
-    public float Volume { get; set; }
+    public string Volume { get; set; }
 
 
     public override string ToString()
